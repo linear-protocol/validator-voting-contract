9f4aa1e9da9cbf47b6552caf4b869f34eb6b7954
Linguists <95207870+linguists@users.noreply.github.com>
2025-05-15 02:10:58 +0000

    test: basic tests

--- tests/test_basics.rs
+++ tests/test_basics.rs
@@ -1,28 +1,40 @@
-// use serde_json::json;
+use serde_json::json;
+use std::time::SystemTime;
 
-// #[tokio::test]
-// async fn test_contract_is_operational() -> Result<(), Box<dyn std::error::Error>> {
-//     let contract_wasm = near_workspaces::compile_project("./").await?;
+#[tokio::test]
+async fn test_contract_is_operational() -> Result<(), Box<dyn std::error::Error>> {
+    let contract_wasm = near_workspaces::compile_project("./").await?;
 
-//     test_basics_on(&contract_wasm).await?;
-//     Ok(())
-// }
+    test_initialization(&contract_wasm).await?;
+    Ok(())
+}
 
-// async fn test_basics_on(contract_wasm: &[u8]) -> Result<(), Box<dyn std::error::Error>> {
-//     let sandbox = near_workspaces::sandbox().await?;
-//     let contract = sandbox.dev_deploy(contract_wasm).await?;
+async fn test_initialization(contract_wasm: &[u8]) -> Result<(), Box<dyn std::error::Error>> {
+    let sandbox = near_workspaces::sandbox().await?;
+    let contract = sandbox.dev_deploy(contract_wasm).await?;
 
-//     let user_account = sandbox.dev_create_account().await?;
+    // Initialize contract
+    let proposal = "test_proposal";
+    let deadline_timestamp_ms = SystemTime::now() + 10 * 60 * 1000;
+    contract
+        .call(contract.id(), "new")
+        .args_json(json!({
+            "proposal": proposal,
+            "deadline_timestamp_md": deadline_timestamp_ms,
+        }))
+        .transact()
+        .await?;
 
-//     let outcome = user_account
-//         .call(contract.id(), "set_greeting")
-//         .args_json(json!({"greeting": "Hello World!"}))
-//         .transact()
-//         .await?;
-//     assert!(outcome.is_success(), "{:#?}", outcome.into_result().unwrap_err());
+    let user_account = sandbox.dev_create_account().await?;
+    let outcome = user_account
+        .call(contract.id(), "vote")
+        .args_json(json!({"is_vote": true}))
+        .transact()
+        .await?;
+    assert!(outcome.is_failure(), "{:#?}", outcome.into_result().unwrap_err());
 
-//     let user_message_outcome = contract.view("get_greeting").args_json(json!({})).await?;
-//     assert_eq!(user_message_outcome.json::<String>()?, "Hello World!");
+    let contract_proposal = contract.view("get_proposal").args_json(json!({})).await?;
+    assert_eq!(contract_proposal.json::<String>()?, proposal);
 
-//     Ok(())
-// }
+    Ok(())
+}
