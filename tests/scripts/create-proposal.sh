753dcd9a943a8d6772bbeb9ef2f57ba744d93360
Linguists <95207870+linguists@users.noreply.github.com>
2025-07-22 10:04:18 +0000

    chore: updae scripts

--- tests/scripts/create-proposal.sh
+++ tests/scripts/create-proposal.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+export MOCK_PROPOSAL_CONTRACT=./res/validator_voting.wasm
 export VOTING_ACCOUNT_ID=mock-proposal-alpha.testnet
 export PROPOSAL_DESCRIPTION="reduce inflation rate"
 export DEADLINE_TIMESTAMP=1753257600000
 
 # create account
-# near account create-account sponsor-by-faucet-service $VOTING_ACCOUNT_ID autogenerate-new-keypair save-to-legacy-keychain network-config testnet create
+near account create-account sponsor-by-faucet-service $VOTING_ACCOUNT_ID autogenerate-new-keypair save-to-legacy-keychain network-config testnet create
 
 # deploy contract
-near contract deploy $VOTING_ACCOUNT_ID use-file ./res/validator_voting.wasm with-init-call new json-args '{"proposal":"'$PROPOSAL_DESCRIPTION'","deadline_timestamp_ms":'$DEADLINE_TIMESTAMP'}' prepaid-gas '100.0 Tgas' attached-deposit '0 NEAR' network-config testnet sign-with-legacy-keychain send
+near contract deploy $VOTING_ACCOUNT_ID use-file $MOCK_PROPOSAL_CONTRACT with-init-call new json-args '{"proposal":"'$PROPOSAL_DESCRIPTION'","deadline_timestamp_ms":'$DEADLINE_TIMESTAMP'}' prepaid-gas '100.0 Tgas' attached-deposit '0 NEAR' network-config testnet sign-with-legacy-keychain send
--- tests/scripts/create-validators.sh
+++ tests/scripts/create-validators.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export MOCK_STAKING_POOL_CONTRACT=./contracts/mock-staking-pool/target/near/mock_staking_pool.wasm
 export OWNER_ID=mock-owner.testnet
 export STAKE_ACCOUNT_ID=mock-staker.testnet
 export VOTING_ACCOUNT_ID=mock-proposal-alpha.testnet
@@ -10,7 +11,7 @@ for i in {1..10}; do
     # near account create-account sponsor-by-faucet-service $VALIDATOR_ID autogenerate-new-keypair save-to-legacy-keychain network-config testnet create
     near account create-account fund-myself $VALIDATOR_ID '2 NEAR' autogenerate-new-keypair save-to-legacy-keychain sign-as $STAKE_ACCOUNT_ID network-config testnet sign-with-legacy-keychain send
     # deploy mock staking pool contract
-    near contract deploy $VALIDATOR_ID use-file ./contracts/mock-staking-pool/target/near/mock_staking_pool.wasm with-init-call new json-args '{"owner_id":"'$OWNER_ID'","stake_public_key":"'$STAKE_PUBLIC_KEY'","voting_account_id":"'$VOTING_ACCOUNT_ID'"}' prepaid-gas '100.0 Tgas' attached-deposit '0 NEAR' network-config testnet sign-with-legacy-keychain send
+    near contract deploy $VALIDATOR_ID use-file $MOCK_STAKING_POOL_CONTRACT with-init-call new json-args '{"owner_id":"'$OWNER_ID'","stake_public_key":"'$STAKE_PUBLIC_KEY'","voting_account_id":"'$VOTING_ACCOUNT_ID'"}' prepaid-gas '100.0 Tgas' attached-deposit '0 NEAR' network-config testnet sign-with-legacy-keychain send
     # stake some NEAR to 
     near contract call-function as-transaction $VALIDATOR_ID deposit_and_stake json-args {} prepaid-gas '200.0 Tgas' attached-deposit '1 NEAR' sign-as $STAKE_ACCOUNT_ID network-config testnet sign-with-legacy-keychain send
 done
